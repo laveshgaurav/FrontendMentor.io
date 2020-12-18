e32b0fa78111f33fc0f844f3cb664093c534f222
Lavesh Gaurav <lavesh.g96@gmail.com>
2020-12-18 15:17:35 +0530

    solved

--- Room-Homepage-Master/main.js
+++ Room-Homepage-Master/main.js
@@ -1,16 +1,56 @@
-var myIndex = 0;
-carousel();
+// var myIndex = 0;
+// carousel();
 
-function carousel() {
+// function carousel() {
+//   var i;
+//   var x = document.getElementsByClassName("img-box-slide");
+//   for (i = 0; i < x.length; i++) {
+//     x[i].style.display = "none";
+//   }
+//   myIndex++;
+//   if (myIndex > x.length) {
+//     myIndex = 1;
+//   }
+//   x[myIndex - 1].style.display = "block";
+//   setTimeout(carousel, 2000);
+// }
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
   var i;
-  var x = document.getElementsByClassName("img-box-slide");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
+  var slides = document.getElementsByClassName("img-box-slide");
+  var textSlide = document.getElementsByClassName("content-slides");
+  if (n > slides.length) {
+    slideIndex = 1;
   }
-  myIndex++;
-  if (myIndex > x.length) {
-    myIndex = 1;
+  if (n < 1) {
+    slideIndex = slides.length;
   }
-  x[myIndex - 1].style.display = "block";
-  setTimeout(carousel, 2000);
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "flex";
+
+  if (n > textSlide.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = textSlide.length;
+  }
+  for (i = 0; i < textSlide.length; i++) {
+    textSlide[i].style.display = "none";
+  }
+  textSlide[slideIndex - 1].style.display = "flex";
 }
